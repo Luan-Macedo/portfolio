3a60b6331f2169e37747872d31363f74995f0097
Luan Costa <lnmaiscedo@gmail.com>
2024-08-29 17:44:12 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Luan-Macedo.png" />
-    <Titulo fontSize={20}>Luan Macedo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luan Macedo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Luan-Macedo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Front-End Developer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
